52311b60552fd8cccb2e22202b9aab3f5f239a8a
Zaira Campazzo <108760980+ZairaCampazzo@users.noreply.github.com>
2023-01-18 02:08:46 -0300

    Initial commit

---
+++ funciones.js
@@ -0,0 +1,20 @@
+
+document.getElementById("fondo_oscuro").addEventListener('click', function() {
+    document.body.style.backgroundColor = '#000000',
+    document.getElementById("titulos").style.backgroundColor = '#CCCCCC';
+    document.getElementById("sobre-mi").style.backgroundColor = '#CCCCCC';
+    document.getElementById("skills").style.backgroundColor = '#CCCCCC';
+    document.getElementById("educacion").style.backgroundColor = '#CCCCCC';
+    document.getElementById("proyectos").style.backgroundColor = '#CCCCCC';
+    document.getElementById("footer").style.backgroundColor = '#CCCCCC';
+});
+
+document.getElementById("fondo_original").addEventListener('click', function() {
+    document.body.style.backgroundColor = '#f1ffcb';
+    document.getElementById("titulos").style.backgroundColor = '#f1ffcb';
+    document.getElementById("sobre-mi").style.backgroundColor = '#f1ffcb';
+    document.getElementById("skills").style.backgroundColor = '#f1ffcb';
+    document.getElementById("educacion").style.backgroundColor = '#f1ffcb';
+    document.getElementById("proyectos").style.backgroundColor = '#f1ffcb';
+    document.getElementById("footer").style.backgroundColor = '#f1ffcb';
+});
